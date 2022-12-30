61ce2a4efacdbfc3f63bba9524b882357143e8e4
sungkmi <sungkmi@gmail.com>
2022-12-30 22:31:01 +0900

    Day1

--- modules/aoc/src/main/scala/dev/sungkm/adventofcode2022/day1to10/Day1.scala
+++ modules/aoc/src/main/scala/dev/sungkm/adventofcode2022/day1to10/Day1.scala
@@ -1,8 +1,33 @@
 package dev.sungkm.adventofcode2022
 package day1to10
 
-import cats.effect.{IO, IOApp}
+import cats.effect.{IO, IOApp, Resource, Sync}
+import cats.effect.std.Console
+import cats.syntax.flatMap.*
+import cats.syntax.functor.*
 
 object Day1 extends IOApp.Simple:
 
-  val run = IO.println("Hello world!")
+  def elves(input: String): Seq[BigInt] =
+    input.split("\n\n").toSeq.map { elf =>
+      elf.split("\n").map(BigInt(_)).sum
+    }
+
+  def part1(input: String) = elves(input).max
+
+  def part2(input: String) = elves(input).sorted.reverse.take(3).sum
+
+  def inputResource[F[_]: Sync](path: String): Resource[F, String] =
+    Resource
+      .fromAutoCloseable(Sync[F].blocking(scala.io.Source.fromFile(path)))
+      .map(_.mkString)
+
+  def printAns[F[_]: Console: Sync](path: String) = inputResource[F](path).use {
+    input =>
+      for
+        _ <- Console[F].println(part1(input))
+        _ <- Console[F].println(part2(input))
+      yield ()
+  }
+
+  val run = printAns[IO]("input/day1")
--- modules/aoc/src/test/scala/dev/sungkm/adventofcode2022/day1to10/Day1Test.scala
+++ modules/aoc/src/test/scala/dev/sungkm/adventofcode2022/day1to10/Day1Test.scala
@@ -6,8 +6,35 @@ import hedgehog.*
 
 class Day1Test extends HedgehogSuite:
 
-  test("test") {
+  val input = """1000
+2000
+3000
+
+4000
+
+5000
+6000
+
+7000
+8000
+9000
+
+10000"""
+
+  test("part1") {
     withMunitAssertions { assertions =>
-      assertions.assertEquals(true, true)
+
+      val expected = BigInt(24000)
+
+      assertions.assertEquals(Day1.part1(input), expected)
+    }
+  }
+
+  test("part2") {
+    withMunitAssertions { assertions =>
+
+      val expected = BigInt(45000)
+
+      assertions.assertEquals(Day1.part2(input), expected)
     }
   }
