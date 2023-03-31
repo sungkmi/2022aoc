d4792015b1c4b0f409e021d584998e38e41ce3da
sungkmi <sungkmi@gmail.com>
2023-03-31 22:12:59 +0900

    update Day13

--- src/main/scala/dev/sungkm/adventofcode2022/day11to20/Day13.scala
+++ src/main/scala/dev/sungkm/adventofcode2022/day11to20/Day13.scala
@@ -8,65 +8,55 @@ import cats.syntax.functor.*
 
 object Day13 extends IOApp.Simple:
 
-  enum Packet:
+  enum Packet extends Ordered[Packet]:
     case PInt(value: BigInt)
     case PList(value: List[Packet])
 
+    infix def compare(that: Packet): Int = (this, that) match
+      case (PInt(x), PInt(y))       => x compare y
+      case (PList(Nil), PList(Nil)) => 0
+      case (PList(Nil), PList(_))   => -1
+      case (PList(_), PList(Nil))   => 1
+      case (PList(x :: xs), PList(y :: ys)) =>
+        val compareHead = x compare y
+        if compareHead != 0 then compareHead
+        else PList(xs) compare PList(ys)
+      case (PInt(_), PList(_)) => this.toPList compare that
+      case (PList(_), PInt(_)) => this compare that.toPList
+
+    def toPList: Packet.PList = this match
+      case PInt(n)   => PList(PInt(n) :: Nil)
+      case pl: PList => pl
+
   object Packet:
-    extension (p: Packet)
-      def toPList: Packet.PList = p match
-        case Packet.PInt(n)   => Packet.PList(Packet.PInt(n) :: Nil)
-        case pl: Packet.PList => pl
-
-    def compare(x: Packet, y: Packet): Int = (x, y) match
-      case (Packet.PInt(x), Packet.PInt(y))       => x.compare(y)
-      case (Packet.PList(Nil), Packet.PList(Nil)) => 0
-      case (Packet.PList(Nil), Packet.PList(_))   => -1
-      case (Packet.PList(_), Packet.PList(Nil))   => 1
-      case (Packet.PList(x :: xs), Packet.PList(y :: ys)) =>
-        val compareHead = compare(x, y)
-        if compareHead =!= 0 then compareHead
-        else compare(Packet.PList(xs), Packet.PList(ys))
-      case (Packet.PInt(_), Packet.PList(_)) => compare(x.toPList, y)
-      case (Packet.PList(_), Packet.PInt(_)) => compare(x, y.toPList)
-
     def parse(s: String): Packet =
-      def parsePInt(str: String, index: Int): (BigInt, Int) =
-        val newIndex = str.indexWhere(!_.isDigit, index)
-        val number   = BigInt(str.slice(index, newIndex))
-        (number, newIndex)
+      def parsePInt(index: Int): (PInt, Int) =
+        val newIndex = s.indexWhere(!_.isDigit, index)
+        val number   = BigInt(s.slice(index, newIndex))
+        (PInt(number), newIndex)
 
-      def parsePList(
-          str: String,
-          index: Int,
-          acc: List[Packet] = Nil,
-      ): (List[Packet], Int) =
-        if index >= str.length || str(index) == ']' then
-          (acc.reverse, index + 1)
+      def parsePList(index: Int, acc: List[Packet] = Nil): (PList, Int) =
+        if index >= s.length || s(index) == ']' then
+          (PList(acc.reverse), index + 1)
         else
-          str(index) match
+          s(index) match
             case '[' =>
-              val (nestedPackets, newIndex) = parsePList(str, index + 1)
-              parsePList(
-                str,
-                newIndex,
-                Packet.PList(nestedPackets) :: acc,
-              )
+              val (nestedPList, newIndex) = parsePList(index + 1)
+              parsePList(newIndex, nestedPList :: acc)
             case c if c.isDigit =>
-              val (number, newIndex) = parsePInt(str, index)
-              parsePList(str, newIndex, Packet.PInt(number) :: acc)
-            case ',' => parsePList(str, index + 1, acc)
+              val (pInt, newIndex) = parsePInt(index)
+              parsePList(newIndex, pInt :: acc)
+            case ',' => parsePList(index + 1, acc)
             case _   => throw new IllegalArgumentException("Invalid input")
 
-      val (packets, _) = parsePList(s, 0)
-      Packet.PList(packets)
+      parsePList(0)._1
 
   def part1(input: String): BigInt = input
     .split("\n\n")
     .zipWithIndex
     .filter: (s, i) =>
       val Array(p1, p2) = s.split("\n").map(Packet.parse)
-      Packet.compare(p1, p2) < 0
+      p1 < p2
     .map(_._2 + 1)
     .sum
 
@@ -75,7 +65,7 @@ object Day13 extends IOApp.Simple:
       input.split("\n").filter(_.size > 0).map(Packet.parse).toVector
     val d1     = Packet.parse("[[2]]")
     val d2     = Packet.parse("[[6]]")
-    val sorted = (packets :+ d1 :+ d2).sortWith(Packet.compare(_, _) < 0)
+    val sorted = (packets :+ d1 :+ d2).sorted
     (sorted.indexOf(d1) + 1) * (sorted.indexOf(d2) + 1)
 
   def inputResource[F[_]: Sync](path: String): Resource[F, String] =
